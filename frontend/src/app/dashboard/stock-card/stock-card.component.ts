41e1e32aef46ad19e3ac1439e145c7299915aa69
FernandaMakiHirose <femahi2020@gmail.com>
2021-06-02 21:41:40 -0300

    Adicionando pastas perdidas

---
+++ frontend/src/app/dashboard/stock-card/stock-card.component.ts
@@ -0,0 +1,28 @@
+import Stock from 'src/app/shared/models/stock-model';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-card',
+  templateUrl: './stock-card.component.html',
+  styleUrls: ['./stock-card.component.css']
+})
+export class StockCardComponent implements OnInit {
+
+  @Input()
+
+  /*declarando uma propriedade*/
+  stock = new Stock();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  //Método de manipulação de string
+  get variation(): string{
+    const localizedVariationString = this.stock.variation.toLocaleString();
+    const prefix = this.stock.variation > 0 ? '+' : '-';
+    return `${prefix} ${localizedVariationString.replace('-', '')}%`;
+  }
+
+}
